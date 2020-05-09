a059496add5f16f29ffdc0aca0e36368fdc3ded1
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-05-09 14:31:44 -0400

    snake-only setup

--- src/PlayerAI_extra.py
+++ src/PlayerAI_extra.py
@@ -12,7 +12,7 @@ from PlayerAI_3 import PlayerAI as Marinov
 
 class PlayerAI(BaseAI):
 
-    def __init__(self, weights = [5,3,2,4,0.03], memo_dic = {}):
+    def __init__(self, weights = [1,0,0,0,0], memo_dic = {}):
         #store previously computed states to reduce redundant computation
         self.memo = memo_dic
         self.timed_out = False
@@ -23,7 +23,7 @@ class PlayerAI(BaseAI):
         #time limit to make sure we don't use too much time
         self.time_limit = 3
         #upper bound on heuristic function for alpha-beta pruning (only for expectimax)
-        self.UPPER_BOUND = 8
+        self.UPPER_BOUND = 4
         self.max_heur = -float('inf')
 
     def getMove(self, grid):
@@ -251,7 +251,7 @@ class PlayerAI(BaseAI):
 
 
     def heuristic(self, grid):
-        vals = [self.monotonicPatternHeuristic(grid), self.monotonicHeuristic(grid), self.clusterHeuristic(grid), self.mergeHeuristic(grid), self.openHeuristic(grid)]
+        vals = [self.snakePatternHeuristic(grid)]
         #print(vals, sum(vals))
         x = sum(vals[i]*self.weights[i] for i in range(len(vals)))
         '''
