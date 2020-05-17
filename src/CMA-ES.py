fcd3f98f687c76a94f9642b60328e568554ffac0
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-05-17 00:23:45 -0400

    CMA-ES more functionality

--- src/CMA-ES.py
+++ src/CMA-ES.py
@@ -13,7 +13,7 @@ def find_means_of_weights(weight_combinations):
     return [np.mean(heur_weight) for heur_weight in weight_combinations]
 
 
-def generate_random_weights_arr(start, end, variables=4, samples=100):
+def generate_data(start, end, variables=4, samples=100):
     """ Creates a variables x samples matrix of random numbers based on range """
     weight_combinations = [None] * variables
     for variable in range(variables):
@@ -24,17 +24,24 @@ def generate_random_weights_arr(start, end, variables=4, samples=100):
     return weight_combinations, means
 
 
-def generate_covariance_matrix(data, population=False):
-    """ Creates either a population or sample covariance matrix based on the data """
-    return np.cov(data, bias=population)
-
-
 def visualize_covariance_matrix(cov_matrix):
-    """ Creates Covariance matrix from weights and their means """
+    """ Displays a visualization of a covariance matrix """
     sn.heatmap(cov_matrix, annot=True, fmt='g')
     plt.show()
 
 
-def generate_samples(covariance_matrix, means, samples=10):
-    """ Generates samples from guassian normal distribution """
+def generate_next_generation_data(data, means, population=False, best_samples=10):
+    """ Greates new data for next generation and their means """
+    data = [0]
+    means = [0]
+    # grab best samples and recalculate means
+    # needs more work
+
+    return data, means
+
+
+def generate_normal_distribution(data, means, cov_matrix=None, population=False, samples=10):
+    """ Finds covariance matrix and returns multivarial normal distribution """
+    covariance_matrix = np.cov(
+        data, bias=population) if not cov_matrix else cov_matrix
     return np.random.Generator.multivariate_normal(covariance_matrix, means, size=samples)
